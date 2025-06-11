cd495be39af5e50b26c20f1a80095f988b1bb8d3
Cesar Nunez <cesar23augusto16@gmail.com>
2025-06-11 16:12:46 -0600

    leer dataset y limpiar un poco

---
+++ README.md
@@ -0,0 +1,38 @@
+# **LSTM and GRU in Fake News Classifier**
+
+## **Prerequisites**
+**Python version 3.8 - 3.11** must be installed on your system.
+
+## **Steps to Run the Project**
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/cesarsiuu2316/LSTM-and-GRU-in-Fake-News-Classifier.git
+```
+
+### 2. Create and Activate a Virtual Environment
+It is recommended to use a virtual environment to install dependencies. Follow these steps:
+
+```bash
+# Create the virtual environment using pip
+python -m venv venv
+```
+Activate the virtual environment
+```bash
+# Activate environment
+venv\Scripts\activate
+```
+
+### 3. **Install Dependencies**
+Once the virtual environment is active, use the `requirements.txt` file from the repository to install the necessary dependencies. Run the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+### 4. Finally, run the following python programs:
+
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,8 +1,56 @@
 import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.models import Sequential
+
+
+# GLOBAL VARIABLES
+vocabulary_size = 10,000  # Size of the vocabulary
+
+
+def read_csv_file(path):
+    print("Reading CSV file...")
+    df = pd.read_csv(path)
+    return df
+
+
+def EDA(df):
+    print("Performing EDA...")
+    print(df.head())
+    print(df.shape)
+    print(df.isnull().sum())
+
+
+def preprocess_data(df):
+    print("Preprocessing data...")
+    df.dropna(inplace=True)  # Remove rows with missing values
+    df.drop(columns = ["Unnamed: 0"], inplace=True)  # Drop unnecessary columns
+    df.reset_index(drop=True, inplace=True)  # Reset index after dropping rows
+    return df
+
 
 def main():
-    print("Hello from lstm-in-fake-news-classifier!")
-    pd.read_csv()
+    # Read dataframe
+    path = "WELFake_Dataset.csv"
+    df = read_csv_file(path)
+
+    # Perform EDA and preprocessing
+    EDA(df)
+    df_clean = preprocess_data(df)
+
+    # Display cleaned data
+    print("Cleaned Data:")
+    print(df_clean.shape)
+
+    # Get features 
+    X = df_clean.drop(columns=["label"])
+    y = df_clean["label"]
+
+    print(tf.__version__)
+
+
 
 if __name__ == "__main__":
     main()
